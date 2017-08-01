44a8940741db98f1c591081ab310096d6bbd7326
Senneer <senneerr@gmail.com>
2017-08-01 23:49:49 +0700

    updated second page

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  $(".order__slider").slick({
+    dots: false,
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1
+  });
+});
+
+$(window).on("load", function() {
+  // $(".stages").mCustomScrollbar();
+  // $(".service").mCustomScrollbar();
+});
--- subcontracts/js/main.js
+++ subcontracts/js/main.js
@@ -1,1 +1,15 @@
 "use strict";
+
+$(document).ready(function () {
+  $(".order__slider").slick({
+    dots: false,
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1
+  });
+});
+
+$(window).on("load", function () {
+  // $(".stages").mCustomScrollbar();
+  // $(".service").mCustomScrollbar();
+});
